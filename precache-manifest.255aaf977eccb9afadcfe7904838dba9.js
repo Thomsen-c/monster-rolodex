8899eebdf41b2162090cbf3965e7e514635a045b
thomsenc <christian.thomsen@aviture.us.com>
2020-06-15 15:00:36 -0500

    Updates

---
+++ precache-manifest.255aaf977eccb9afadcfe7904838dba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c0a29db0e216542316f4ea47b57265",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6a702c927f5ca18379ac",
+    "url": "/monsters-rolodex/static/css/main.805ab4aa.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a702c927f5ca18379ac",
+    "url": "/monsters-rolodex/static/js/main.5b8d03c4.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
